42e1c309ffbe631ed388216952c7ca5422252d7d
Fangfang.Xu <Fangfang.Xu@HZW001-W702WV0L.Geely.Auto>
2024-01-22 10:45:26 +0800

    update

--- nutrient-designpattern/src/main/java/io/github/icodegarden/nutrient/designpattern/factorymethod/GrapeGardener.java
+++ nutrient-designpattern/src/main/java/io/github/icodegarden/nutrient/designpattern/factorymethod/GrapeGardener.java
@@ -4,6 +4,6 @@ public class GrapeGardener implements FruitGardener
 {
     public Fruit factory()
     {
-        return new Apple();
+        return new Grape();
     }
 }
--- nutrient-designpattern/src/main/java/io/github/icodegarden/nutrient/designpattern/factorymethod/StrawberryGardener.java
+++ nutrient-designpattern/src/main/java/io/github/icodegarden/nutrient/designpattern/factorymethod/StrawberryGardener.java
@@ -4,6 +4,6 @@ public class StrawberryGardener implements FruitGardener
 {
     public Fruit factory()
     {
-        return new Apple();
+        return new Strawberry();
     }
 }
